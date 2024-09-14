01b0f1f60de848a7a03c70fe4fd936d5c867b55c
Rohan Uke <ukerohan7@gmail.com>
2024-09-15 00:14:45 +0530

    add info about list

--- 05-list/01list.py
+++ 05-list/01list.py
@@ -1,3 +1,9 @@
+#List
+#1.Ordered and mutable collection of elements.
+#2.Can contain elements of different data types.
+#3.Elements can be accessed, modified, added, or removed by index.
+#4.Created using square brackets [].
+#5.Versatile and commonly used for storing sequences.
 lst=[]
 print(type(lst))
 
